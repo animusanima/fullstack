fda423de909d73401f25f75008e7c3ffcbb01877
animusanima <molockokannzocko@gmail.com>
2024-03-26 21:37:04 +0100

    Added test for creating a todo and storing it in the local database file
    - Adding method for cleaning up test data
    - Calling beforeEach and afterAll for clean up
    - Added store.test.json

---
+++ apps/server/src/db-client/tododb.test.ts
@@ -0,0 +1,22 @@
+import { afterAll, expect, test } from "vitest";
+import { Database } from "./tododb";
+import { beforeEach } from "node:test";
+import path from "node:path";
+import { promises as fs } from "fs";
+
+const TEST_FILE_PATH = "./store.test.json";
+const database = new Database(TEST_FILE_PATH);
+
+async function resetTestFile(): Promise<void> {
+  const filePath = path.join(__dirname, TEST_FILE_PATH);
+  await fs.writeFile(filePath, JSON.stringify([]), "utf-8");
+};
+
+beforeEach(resetTestFile);
+afterAll(resetTestFile);
+
+test("create a todo will add todo to list", async () => {
+  const todo = await database.create({ title: "new Todo", completed: false });
+  expect(todo.title).toBe("new Todo");
+  expect(todo).toHaveProperty("completed", false);
+});
