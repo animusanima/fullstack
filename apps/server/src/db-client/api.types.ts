fff2b4acc73a091cd08ba8ec4e69cf9044ab0745
animusanima <molockokannzocko@gmail.com>
2024-03-26 21:33:33 +0100

    Moved API Types to own file api.types.ts

---
+++ apps/server/src/db-client/api.types.ts
@@ -0,0 +1,28 @@
+import type { Todo } from "shared";
+
+export type ReadJsonResult = {
+  success: true;
+  todos: Todo[]
+} | {
+  success: false;
+  error: Error
+};
+
+export type CreateArgs = Omit<Todo, "id">;
+
+export type FindUniqueArgs = {
+  id: Todo["id"];
+}
+
+export type UpdateArgs = {
+  id: Todo["id"];
+  data: Partial<Omit<Todo, "id">>;
+}
+
+export type DeleteArgs = {
+  id: Todo["id"];
+}
+
+export type DeleteManyArgs = {
+  ids: Array<Todo["id"]>
+} | { clearAll: true };
