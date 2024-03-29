3e4e535b942d7deb50e80d12f7e61ed13d9f5bcb
animusanima <molockokannzocko@gmail.com>
2024-03-29 11:37:00 +0100

    Refactoring code in todo.notification.ts
    - Using references from elements.ts for accessing controls
    - Remove getElements from imports

--- apps/client/src/utils/todo.notification.ts
+++ apps/client/src/utils/todo.notification.ts
@@ -1,29 +1,22 @@
-import { elements, getElement } from "./elements.ts";
+import { elements } from "./elements.ts";
 
 const TODO_SAVED_SUCCESSFULLY = "Todo has been saved successfully";
 const TODO_DELETED_SUCCESSFULLY = "Todo has been deleted successfully";
 
 function setNotificationText(textToShow: string): void {
-  const notificationText = getElement<HTMLParagraphElement>("#notificationText");
-  notificationText.textContent = textToShow;
+  elements.notificationText.textContent = textToShow;
 }
 
 function showSuccessNotification(): void {
-  const notification = getElement<HTMLDivElement>("#notification");
-  notification.className = "notification is-success";
-
-  const notificationArea = getElement<HTMLDivElement>("#notificationArea");
   setNotificationText(TODO_SAVED_SUCCESSFULLY);
-  notificationArea.removeAttribute("class");
+  elements.notification.className = "notification is-success";
+  elements.notificationArea.removeAttribute("class");
 }
 
 function showDeletedNotification(): void {
-  const notification = getElement<HTMLDivElement>("#notification");
-  notification.className = "notification is-danger";
-
-  const notificationArea = getElement<HTMLDivElement>("#notificationArea");
   setNotificationText(TODO_DELETED_SUCCESSFULLY);
-  notificationArea.removeAttribute("class");
+  elements.notification.className = "notification is-danger";
+  elements.notificationArea.removeAttribute("class");
 }
 
 export const NotificationHelper = {
