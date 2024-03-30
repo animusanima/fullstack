6547c853e4bf0b24b6515f1f2be710db3816bed6
animusanima <molockokannzocko@gmail.com>
2024-03-30 19:41:15 +0100

    Removed todo comments for implemented features

--- apps/client/src/main.ts
+++ apps/client/src/main.ts
@@ -1,11 +1,7 @@
 import { todoElements } from "./todo/todo.elements.ts";
 import { storage } from "./todo/todo.storage.ts";
 
-// TODO: Implement updating todos
-
 // TODO: Implement deletion of many todos
-
-// TODO: Implement moving unfinished todos to completed list
 
 todoElements.createButton.onclick = async (): Promise<void> => {
   await storage.createTodo(todoElements.todoTitle.value);
